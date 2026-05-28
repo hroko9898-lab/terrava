512e9a973f08cd27a43ebe9b7096f12a0c41d26a
Hristiyan <hroko9898@gmail.com>
2026-05-28 12:40:36 +0300

    Add mountain markers to map

--- app/components/TerravaMap.tsx
+++ app/components/TerravaMap.tsx
@@ -54,6 +54,61 @@ export default function TerravaMap() {
           }
         )
         .addTo(map);
+
+      const mountains = [
+        {
+          name: "Рила",
+          position: [42.17, 23.58],
+          link: "/mountains/rila",
+        },
+        {
+          name: "Пирин",
+          position: [41.76, 23.42],
+          link: "/mountains/pirin",
+        },
+        {
+          name: "Родопи",
+          position: [41.72, 24.75],
+          link: "/mountains/rodopi",
+        },
+        {
+          name: "Стара планина",
+          position: [42.72, 24.92],
+          link: "/mountains/stara-planina",
+        },
+        {
+          name: "Витоша",
+          position: [42.56, 23.28],
+          link: "/mountains/vitosha",
+        },
+        {
+          name: "Странджа",
+          position: [42.05, 27.65],
+          link: "/mountains/strandzha",
+        },
+      ];
+
+      mountains.forEach((mountain) => {
+        L.circleMarker(mountain.position as [number, number], {
+          radius: 9,
+          color: "#3b2416",
+          weight: 3,
+          fillColor: "#f3eadb",
+          fillOpacity: 1,
+        })
+          .addTo(map)
+          .bindPopup(
+            `
+              <div style="font-family: sans-serif; min-width: 140px;">
+                <strong style="font-size: 16px;">${mountain.name}</strong>
+                <br />
+                <a href="${mountain.link}" style="color: #3b2416; font-weight: 700;">
+                  Отвори страницата
+                </a>
+              </div>
+            `
+          );
+      });
     }
 
     loadMap();
@@ -68,7 +123,7 @@ export default function TerravaMap() {
   return (
     <div
       ref={mapRef}
-     className="relative z-0 h-[520px] w-full overflow-hidden rounded-[2rem]"
+      className="relative z-0 h-[520px] w-full overflow-hidden rounded-[2rem]"
     />
   );
 }
